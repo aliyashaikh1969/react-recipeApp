9a6a6c847ef4d5de32b8bba663ca6788e8f6d9e5
aliyashaikh1969 <aliyashaikh1969@gmail.com>
2025-06-04 15:31:55 +0530

    removing unwanted

--- src/Pages/Details/Details.jsx
+++ src/Pages/Details/Details.jsx
@@ -1,7 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { GlobalContext } from '../../context/Context'
-import { Favourite } from '../Favoutite/Favourite'
 
 export const Details = () => {
 
